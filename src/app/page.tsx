337755233fc3d70d70932968f2e0a06722ee0273
aneeshaa1 <acharyannie@gmail.com>
2026-04-10 12:05:23 -0400

    set up vapi voice agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import VapiWidget from "./components/VapiWidget";
+import { env } from "@/config/env";
 
 export default async function Home() {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-zinc-900 to-black">
       <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
-        <VapiWidget apiKey="YOUR_API_KEY" assistantId="YOUR_ASSISTANT_ID" />
+        <VapiWidget apiKey={env.VAPI_PUBLIC_KEY} assistantId={env.VAPI_ASSISTANT_ID} />
       </div>
     </div>
   );
